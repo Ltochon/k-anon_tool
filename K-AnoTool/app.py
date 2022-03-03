88bde6fc34f8d7744ebb5951b7eb7eedd9a40025
Ltochon <ltochon@live.fr>
2022-03-03 14:17:47 +0100

    update flask

--- K-AnoTool/app.py
+++ K-AnoTool/app.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import timedelta
 import os
+import sys
 from numpy import save
 import pandas as pd
 from flask import Flask, redirect, render_template, request, send_from_directory, url_for, flash
@@ -8,6 +9,9 @@ from werkzeug.utils import secure_filename
 from upload.upload import upload
 
 app = Flask(__name__)
+app.register_blueprint(upload, url_prefix = "/upload")
+app.secret_key = "super key"
+app.permanent_session_lifetime = timedelta(days=365)
 # enable debugging mode
 app.config["DEBUG"] = True
 
@@ -33,8 +37,13 @@ def uploadFiles():
           file_path = os.path.join(basedir, app.config['UPLOAD_FOLDER'], uploaded_file.filename).replace("\\","/")
           # set the file path
           uploaded_file.save(file_path)
+          print_csv(file_path)
           # save the file
-      return redirect(url_for('csv'))
+      return print_csv(file_path)
+
+def print_csv(path):
+    data = pd.read_csv(path)
+    return render_template('upload.html', tables=[data.to_html(classes='data', header="true")])
 
 if (__name__ == "__main__"):
      app.run(port = 5000)
--- K-AnoTool/upload/upload.py
+++ K-AnoTool/upload/upload.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 upload = Blueprint("upload", __name__, static_folder="static", template_folder="templates")
 
-@upload.route("/upload")
+@upload.route("/")
 def upload_page():
     return render_template("upload.html")
