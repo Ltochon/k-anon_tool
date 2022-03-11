afdd3ce5386c4ff5717d221b99f7462901349511
Ltochon <ltochon@live.fr>
2022-03-11 20:28:02 +0100

    bug fixed

--- test_algo/datafly_v1_weighted.py
+++ test_algo/datafly_v1_weighted.py
@@ -19,18 +19,17 @@ def order_occu(df,qid):
 
 def generalize(df,column):
     #custom generalization to do
-    max_gen = 0
     if(column == "13"):
         df[column] = df["15"]
     elif(column == "10"):
         df[column] = "*"
-    else:
-        max_gen = 1
-    return df,max_gen
+    return df
 
-def fly(df,qid,w_qid,k):
+def fly(df,qid,w_qid,k,max_gen):
+    tab_gen = len(qid)*[0] #array to check how deep in generalization we are
     index_max = 1
     while(check_ano(df,qid) < k):
+        print("ok")
         compute_occu = order_occu(df,qid)
         val = compute_occu[0]
         total = compute_occu[1]
@@ -40,17 +39,27 @@ def fly(df,qid,w_qid,k):
                 tab_distinct.append(0)
             else:
                 tab_distinct.append(len(total[q]))
-        print(check_ano(df,qid))
         #WEIGHTS
-        for w in range(5,0,-1):
-            indices = [i for i, x in enumerate(w_qid) if x == w]
-            if(len(indices) > 0):
-                index_max_local = max(range(len(indices)), key=[tab_distinct[i] for i in indices].__getitem__)
-                if([tab_distinct[i] for i in indices][index_max_local] > 0):
-                    index_max = tab_distinct.index([tab_distinct[i] for i in indices][index_max_local])
-                    break
-        gen = generalize(df,qid[index_max])
-        if(gen[1] > 0):
+        find = False
+        if(tab_gen != max_gen):
+            for w in range(5,0,-1):
+                indices = [i for i, x in enumerate(w_qid) if x == w]
+                if(len(indices) > 0):
+                    for indice in indices:
+                        index_max_local = max(range(len(indices)), key=[tab_distinct[i] for i in indices].__getitem__)
+                        if([tab_distinct[i] for i in indices][index_max_local] > 0):
+                            index_max = tab_distinct.index([tab_distinct[i] for i in indices][index_max_local])
+                            if(tab_gen[index_max] < max_gen[index_max]):
+                                find = True
+                                break
+                            else:
+                                indices.pop(index_max)
+                    if(find == True):
+                        break
+            tab_gen[index_max] = tab_gen[index_max] + 1 #add 1 to depth of qid's generalization
+            gen = generalize(df,qid[index_max])
+            df = gen
+        else:
             #delete all seq which occu < k
             for q in range(0,len(qid)):
                 for i in range(0,len(total[q])):
@@ -58,19 +67,17 @@ def fly(df,qid,w_qid,k):
                         df = df[getattr(df,qid[q]) != val[q][i]]
                     else:
                         break
-                break
-        else:
-            df = gen[0]
     return df
 
 #Run
-def run(df,k,qid,w_qid):
-    df_final = fly(df,qid,w_qid,k)
+def run(df,k,qid,w_qid,max_gen):
+    df_final = fly(df,qid,w_qid,k,max_gen)
     print(df_final)
     return check_ano(df_final,qid)
 
 df = read_file('test_algo/data/adult.csv', ';')
-k = 16193
+k = 22000
 qid = ["10","13"]
 w_qid = [4,2]
-print(run(df,k,qid,w_qid))
+max_gen = [1,1]
+print(run(df,k,qid,w_qid,max_gen))
