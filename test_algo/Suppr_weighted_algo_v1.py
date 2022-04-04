75d9199fab40495daba5d64d03b9f9023ba4c33e
Ltochon <ltochon@live.fr>
2022-04-04 22:44:30 +0200

    working algo sup_weight with len(qid) > 1 and optimized

--- test_algo/Suppr_weighted_algo_v1.py
+++ test_algo/Suppr_weighted_algo_v1.py
@@ -1,5 +1,8 @@
+from cmath import nan
+from numpy import NaN
 import pandas as pd
-from operator import index, itemgetter 
+import itertools
+
 def read_file(path, delim):
     csv = pd.read_csv(path, sep=delim)
     return csv
@@ -9,40 +12,69 @@ def check_ano(df,qid):
     dups_shape = datas_qid.pivot_table(columns=qid, aggfunc='size')
     return min(dups_shape)
 
+def get_class(df,qid):
+    datas_qid = df[qid]
+    dups_shape = datas_qid.pivot_table(columns=qid, aggfunc='size')
+    return dups_shape
+
 def generalize(df,qid,lvl):
     if qid == "zip":
         if lvl == 1:
             df["zip"] = df["canton"]
         elif lvl == 2:
             df["zip"] = df["pays"]
+    elif qid == "age":
+        if lvl == 1:
+            df["age"] = df["age_dizaine"]
+        elif lvl == 2:
+            df["age"] = df["age_split"]
     return df
 
 def occu(df,qid):
+    tab_dict = []
     for q in qid:
         index_tab = df[q].value_counts(ascending = True).index.tolist()
         occu_tab = df[q].value_counts(ascending = True).tolist()
-    return dict(zip(index_tab,occu_tab))
+        tab_dict.append(dict(zip(index_tab,occu_tab)))
+    return tab_dict
 
-def algo(df,qid,max_gen,weigths,k):
+def algo(df_init,qid,max_gen,weigths,k):
+    list_comb = []
+    for g in max_gen:
+        list_comb.append(list(range(0,g+1)))
+    list_comb = list(itertools.product(*list_comb))
     print("Start")
     cost = []
-    for q in range(0,len(qid)):
-        for g in range(0,max_gen[q]+1):
-            df = generalize(df,qid[q],g)
-            count_supp = 0
-            if(check_ano(df,qid) < k):
-                dict_occu = occu(df,qid)
-                for d in dict_occu:
-                    if(dict_occu[d] < k):
-                        count_supp += dict_occu[d]
-            print(f"\nQID : {qid[q]}, lvl of generalization : {g}, number of suppression : {count_supp}, total cost : {count_supp * sum(weigths[q]) + len(df) * sum(weigths[q][0:g])}")
-            cost.append(count_supp * sum(weigths[q]) + len(df) * sum(weigths[q][0:g])) 
+    ok = NaN
+    for c in list_comb:
+        if(ok is not NaN):
+            if(len([item1 for item1, item2 in zip(c, ok) if item1 >= item2]) == len(qid)):
+                continue
+        df = df_init.copy()
+        for q in range(0,len(qid)):
+            df = generalize(df,qid[q],c[q])
+        count_supp = 0
+        if(check_ano(df,qid) < k):
+            size_class = get_class(df,qid)
+            for s in size_class:
+                if(s < k):
+                    count_supp += s
+        sum_w = 0
+        for q2 in range(0,len(qid)):
+            sum_w += sum(weigths[q][0:c[q2]])
+        print(f"\nQID : {qid}, lvl of generalization : {c}, number of suppression : {count_supp}, total cost : {count_supp * sum(sum(weigths,[])) + (len(df)-count_supp) * sum_w}, k without suppression = {check_ano(df,qid)}")
+        cost.append(count_supp * sum(sum(weigths,[])) + (len(df)-count_supp) * sum_w)
+        if(count_supp == 0):
+            ok = c
+        else:
+            ok = NaN
+            #break
     return cost
 
 
 df = read_file("test_algo/data/complete_data_test.csv",",")
-qid = ["zip"]
-max_gen = [2]
-weigths = [[2,3]]
+qid = ["zip","age"]
+max_gen = [2,2]
+weigths = [[2,3],[3,4]]
 k = 10
 algo(df,qid,max_gen,weigths,k)
--- test_algo/data/create_data.py
+++ test_algo/data/create_data.py
@@ -3,9 +3,12 @@ from random import randint
 import numpy as np
 import pandas as pd
 zip = ["1001","1002","1003","1004","1201","1202","1203","1204","01","01","01","02","02","02"]
+age, age2, age3 = [],[],[]
 zip_list,canton_list,pays_list = [],[],[]
 for x in range(200):
     e = randint(0,len(zip)-1)
+    a = randint(10,50)
+    age.append(a)
     zip_list.append(zip[e])
     if(int(zip[e]) < 2):
         canton = "Ain"
@@ -19,12 +22,28 @@ for x in range(200):
     else:
         canton = "GE"
         pays = "CH"
+    if(a < 20):
+        age2.append("<20")
+        age3.append("<30")
+    elif(a < 30):
+        age2.append("<30")
+        age3.append("<30")
+    elif(a < 40):
+        age2.append("<40")
+        age3.append(">=40")
+    else:
+        age2.append(">=40")
+        age3.append(">=40")
+    
     canton_list.append(canton)
     pays_list.append(pays)
 df = pd.DataFrame()
 df["zip"] = zip_list
 df["canton"] = canton_list
 df["pays"] = pays_list
+df["age"] = age
+df["age_dizaine"] = age2
+df["age_split"] = age3
 cwd = os.getcwd()
 path = cwd + "/test_algo/data/complete_data_test.csv"
 df.to_csv(path)
