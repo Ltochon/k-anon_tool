93cebc308139b4bd3a3e946bd46b258dfebb5edf
Ltochon <ltochon@live.fr>
2022-04-11 08:35:41 +0200

    create samarati function

--- test_algo/samarati_tree.py
+++ test_algo/samarati_tree.py
@@ -1,20 +1,19 @@
-import itertools
-import numpy
-max_gen = [3,4]
-current = [tuple(len(max_gen)*[0])]
-max_tuple = tuple(max_gen)
-tree = []
-while current != []:
-    tree.append(current) 
-    copy = current
-    next_lvl = []
-    for elem in range(len(copy)):
-        l = list(copy[elem])
-        for i in range(len(l)):
-            new_l = l.copy()
-            new_l[i] += 1
-            if(new_l[i] <= max_gen[i]):
-                if(tuple(new_l) not in next_lvl):
-                    next_lvl.append(tuple(new_l))  
-    current = next_lvl
-print(tree)  
+def samarati_tree(max_gen):
+    current = [tuple(len(max_gen)*[0])]
+    tree = []
+    while current != []:
+        tree.append(current) 
+        copy = current
+        next_lvl = []
+        for elem in range(len(copy)):
+            l = list(copy[elem])
+            for i in range(len(l)):
+                new_l = l.copy()
+                new_l[i] += 1
+                if(new_l[i] <= max_gen[i]):
+                    if(tuple(new_l) not in next_lvl):
+                        next_lvl.append(tuple(new_l))  
+        current = next_lvl
+    return tree
+
+print(samarati_tree([3,2]))
